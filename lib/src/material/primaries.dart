b8bd7e153c05788873258ef8a72779e86642fcf9
rafael <r.amizes@gmail.com>
2021-05-17 19:30:32 -0300

    erase non-PDD TODO's

--- lib/src/material/primaries.dart
+++ lib/src/material/primaries.dart
@@ -6,14 +6,13 @@ import '../swatch_base.dart';
 /// See also
 /// - [primaries](https://api.flutter.dev/flutter/material/Colors/primaries-constant.html)
 class Primaries extends SwatchBase {
+  // @todo #24 Primaries will be fully implemented when the following colors are
+  //  available: red, pink, purple, deep purple, indigo, blue, light blue, cyan,
+  //  teal, green, light green, lime, yellow, amber, orange, deep orange.
+
   /// Default primary colors.
   ///
   /// Obs: many colors have not been implemented yet; see @todo #24 for details.
   /// Available colors: brown, blue-grey.
-
-  // @todo #24 Primaries will be fully implemented when the following
-  // colors are available:
-  // - red, pink, purple, deep purple, indigo, blue, light blue, cyan, teal,
-  //   green, light green, lime, yellow, amber, orange, deep orange.
   const Primaries() : super(const [Brown(), BlueGrey()]);
 }
