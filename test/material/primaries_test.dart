afcc8a4fbe65bedb83f27f8edec4b0cd1843d066
rafael <r.amizes@gmail.com>
2021-05-17 19:46:37 -0300

    fix malformed pdd 'todo' marker

--- test/material/primaries_test.dart
+++ test/material/primaries_test.dart
@@ -4,19 +4,19 @@ import 'package:flutter_test/flutter_test.dart';
 
 /// [primaries](https://api.flutter.dev/flutter/material/Colors/primaries-constant.html)
 void main() {
-  /** 
-   * @todo #24 when all primary colores are available, uncoment the code
-   *  below to perform a complete test over Primaries.
-   *  expect(colors.length, Colors.primaries.length);
-   *  for (var i = 0; i < colors.length; i++) {
-   *    expect(colors[i], Colors.primaries[i].shade500);
-   *  }
-   */
   group('Primaries Swatch', () {
     const primaries = Primaries();
     test('Default', () {
       final colors = primaries.colors.toList();
       expect(colors, [Colors.brown.shade500, Colors.blueGrey.shade500]);
+      /** 
+       * @todo #24 when all primary colores are available, replace the code
+       *  above with the code below:
+       *  expect(colors.length, Colors.primaries.length);
+       *  for (var i = 0; i < colors.length; i++) {
+       *  expect(colors[i], Colors.primaries[i].shade500);
+       *  }
+       */
     });
   });
 }
