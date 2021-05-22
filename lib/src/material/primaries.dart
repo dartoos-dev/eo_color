03b019abad5437bcb75dbe6af5c49440a9839d47
rafael <r.amizes@gmail.com>
2021-05-22 10:14:02 -0300

    (#59): Primaries swatch completion

--- lib/src/material/primaries.dart
+++ lib/src/material/primaries.dart
@@ -6,13 +6,46 @@ import 'package:eo_color/swatches.dart';
 /// See also
 /// - [primaries](https://api.flutter.dev/flutter/material/Colors/primaries-constant.html)
 class Primaries extends SwatchBase {
-  // @todo #24 Primaries will be fully implemented when the following colors are
-  //  available: red, pink, purple, deep purple, indigo, blue, light blue, cyan,
-  //  teal, green, light green, lime, yellow, amber, orange, deep orange.
-
-  /// Default primary colors.
+  ///  All primary colors except grey.
   ///
-  /// Obs: many colors have not been implemented yet; see @todo #24 for details.
-  /// Available colors: brown, blue-grey.
-  const Primaries() : super(const [Brown(), BlueGrey()]);
+  /// There are 18 valid indexes
+  /// - 0, red
+  /// - 1, pink
+  /// - 2, purple
+  /// - 3, deep purple
+  /// - 4, indigo
+  /// - 5, blue
+  /// - 6, light blue
+  /// - 7, cyan
+  /// - 8, teal
+  /// - 9, green
+  /// - 10, light green
+  /// - 11, lime
+  /// - 12, yellow
+  /// - 13, amber
+  /// - 14, orange
+  /// - 15, deep orange
+  /// - 16, brown,
+  /// - 17, blue-grey
+  const Primaries()
+      : super(const [
+          Red(),
+          Pink(),
+          Purple(),
+          DeepPurple(),
+          Indigo(),
+          Blue(),
+          LightBlue(),
+          Cyan(),
+          Teal(),
+          Green(),
+          LightGreen(),
+          Lime(),
+          Yellow(),
+          Amber(),
+          Orange(),
+          DeepOrange(),
+          Brown(),
+          BlueGrey()
+        ]);
 }
--- test/material/primaries_test.dart
+++ test/material/primaries_test.dart
@@ -8,15 +8,10 @@ void main() {
     const primaries = Primaries();
     test('Default', () {
       final colors = primaries.colors.toList();
-      expect(colors, [Colors.brown.shade500, Colors.blueGrey.shade500]);
-      /** 
-       * @todo #24 when all primary colores are available, replace the code
-       *  above with the code below:
-       *  expect(colors.length, Colors.primaries.length);
-       *  for (var i = 0; i < colors.length; i++) {
-       *  expect(colors[i], Colors.primaries[i].shade500);
-       *  }
-       */
+      expect(colors.length, Colors.primaries.length);
+      for (var i = 0; i < colors.length; i++) {
+        expect(colors[i], Colors.primaries[i].shade500);
+      }
     });
   });
 }
