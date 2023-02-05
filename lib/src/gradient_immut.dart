4a905b2401feee8e122fddea052d39d7fbb67174
Rafael Amizes <r.amizes@gmail.com>
2023-02-05 14:09:54 -0300

    fix

--- lib/src/gradient_immut.dart
+++ lib/src/gradient_immut.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
+import 'package:eo_color/gradients.dart' as eo_color;
 import 'package:eo_color/palettes.dart';
 import 'package:eo_color/swatches.dart';
 
 /// Convenience [Gradient] that retrieves immutable lists of colors.
-abstract class GradientImmut extends SwatchBase implements Gradient {
+abstract class GradientImmut extends SwatchBase implements eo_color.Gradient {
   /// [palettes] is a source of colors to be transformed into an immutable list.
   const GradientImmut(Iterable<Palette> palettes) : super(palettes);
 
